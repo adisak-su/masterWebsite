69b272268121598549e1661b4804d15e448d0bee
Adisak Supatanasinkasem <adisak.55555@gmail.com>
2024-09-07 08:23:11 +0700

    Update index.php

--- adminPages/dashboard/index.php
+++ adminPages/dashboard/index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta name="apple-touch-fullscreen" content="yes" />
+    <!-- <meta name="apple-touch-fullscreen" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" /> -->
 
     <?php //include "../includes/apple-meta.php"; 
     ?>
