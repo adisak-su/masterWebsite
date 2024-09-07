5f6dbbde83ee8c1a698129df6e85ce31986d28e7
Adisak Supatanasinkasem <adisak.55555@gmail.com>
2024-09-07 08:37:00 +0700

    commit

--- favicons.php
+++ favicons.php
@@ -1,6 +1,5 @@
 
 <link rel="apple-touch-icon" sizes="192x192" href="assets/favicons/favicon-192x192.png">
-<link rel="touch-icon" sizes="192x192" href="assets/favicons/favicon-192x192.png">
 <link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
 
     <!-- <link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16x16.png"> -->
