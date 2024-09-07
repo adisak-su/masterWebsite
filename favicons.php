3c48414cdf0aea8525963c38b81255af22a054de
Adisak Supatanasinkasem <adisak.55555@gmail.com>
2024-09-07 08:31:24 +0700

    commit

--- favicons.php
+++ favicons.php
@@ -1,11 +1,9 @@
 
-    <!-- <link rel="apple-touch-icon" sizes="180x180" href="assets/favicons/apple-touch-icon-180x180.png"> -->
-    <link rel="apple-touch-icon" sizes="192x192" href="assets/favicons/favicon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
+<link rel="apple-touch-icon" sizes="192x192" href="assets/favicons/favicon-192x192.png">
+<link rel="touch-icon" sizes="192x192" href="assets/favicons/favicon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
+
     <!-- <link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16x16.png"> -->
     <!-- <link rel="mask-icon" href="assets/favicons/safari-pinned-tab.svg" color="#5bbad5"> -->
     <!-- <link rel="shortcut icon" href="assets/favicons/favicon.ico"> -->
     
-    <!-- <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="assets/img/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff"> -->
